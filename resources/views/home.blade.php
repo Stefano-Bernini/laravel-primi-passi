1cf839380c4bd1f87d62d6cd43a51c1a7eabce3c
Stefano-Bernini <stefanobernini26@gmail.com>
2023-07-21 01:56:29 +0200

    collegato bootstrap e stampato hello world

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <title>Laravel</title>
     </head>
     <body>
-        
+        <div class="container vh-100 d-flex justify-content-center align-items-center">
+            <div class="row">
+                <div class="col">
+                    <h1>Hello Wolrd!</h1>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
